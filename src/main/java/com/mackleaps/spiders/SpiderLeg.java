988f3b8e08a0e36858488f18cbc89ebc9f61525d
lucashort7 <lucashort7@gmail.com>
2018-02-08 12:39:11 -0200

    SpiderLeg.java and SpiderTest.java

--- src/main/java/com/mackleaps/spiders/SpiderLeg.java
+++ src/main/java/com/mackleaps/spiders/SpiderLeg.java
@@ -7,72 +7,86 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+
+import java.net.InetSocketAddress;
+import java.net.Proxy;
 import java.util.LinkedList;
 import java.util.List;
 
-public class SpiderLeg
-{
+
+
+public class SpiderLeg {
+
     // We'll use a fake USER_AGENT so the web server thinks the robot is a normal web browser.
-    private static final String USER_AGENT =
-            "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.112 Safari/535.1";
+    private static final String USER_AGENT
+            = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.112 Safari/535.1";
     private List<String> links = new LinkedList<String>();
     private Document htmlDocument;
 
-
     /**
-     * This performs all the work. It makes an HTTP request, checks the response, and then gathers
-     * up all the links on the page. Perform a searchForWord after the successful crawl
+     * This performs all the work. It makes an HTTP request, checks the
+     * response, and then gathers up all the links on the page. Perform a
+     * searchForWord after the successful crawl
      *
-     * @param url
-     *            - The URL to visit
+     * @param url - The URL to visit
      * @return whether or not the crawl was successful
      */
-    public boolean crawl(String url)
-    {
-        try
-        {
-            Connection connection = Jsoup.connect(url).userAgent(USER_AGENT);
-            Document htmlDocument = connection.get();
-            this.htmlDocument = htmlDocument;
-            if(connection.response().statusCode() == 200) // 200 is the HTTP OK status code
-            // indicating that everything is great.
-            {
-                System.out.println("\n**Visiting** Received web page at " + url);
-            }
-            if(!connection.response().contentType().contains("text/html"))
-            {
-                System.out.println("**Failure** Retrieved something other than HTML");
-                return false;
-            }
-            Elements linksOnPage = htmlDocument.select("a[href]");
-            System.out.println("Found (" + linksOnPage.size() + ") links");
-            for(Element link : linksOnPage)
-            {
-                this.links.add(link.absUrl("href"));
-            }
-            return true;
-        }
-        catch(IOException ioe)
-        {
-            // We were not successful in our HTTP request
+    public boolean crawl(String url) {
+
+        System.out.println("BATATA 2");
+        Connection connection = null;
+        Document htmlDocument = null;
+
+        Proxy proxy = new Proxy(
+                Proxy.Type.HTTP,
+                InetSocketAddress.createUnresolved("172.16.0.10", 3128)
+        );
+
+        try {
+            connection = Jsoup.connect(url).userAgent(USER_AGENT).proxy(proxy);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
             return false;
         }
+
+        try {
+            htmlDocument = connection.get();
+        } catch (Exception ex) {
+            System.out.println("lol3");
+            ex.printStackTrace();
+            return false;
+        }
+
+        this.htmlDocument = htmlDocument;
+        System.out.println(this.htmlDocument);
+        if (connection.response().statusCode() == 200) // 200 is the HTTP OK status code
+        // indicating that everything is great.
+        {
+            System.out.println("\n**Visiting** Received web page at " + url);
+        }
+        if (!connection.response().contentType().contains("text/html")) {
+            System.out.println("**Failure** Retrieved something other than HTML");
+            return false;
+        }
+        Elements linksOnPage = htmlDocument.select("a[href]");
+        System.out.println("Found (" + linksOnPage.size() + ") links");
+        for (Element link : linksOnPage) {
+            this.links.add(link.absUrl("href"));
+        }
+        return true;
+
     }
 
-
     /**
-     * Performs a search on the body of on the HTML document that is retrieved. This method should
-     * only be called after a successful crawl.
+     * Performs a search on the body of on the HTML document that is retrieved.
+     * This method should only be called after a successful crawl.
      *
-     * @param searchWord
-     *            - The word or string to look for
+     * @param searchWord - The word or string to look for
      * @return whether or not the word was found
      */
-    public boolean searchForWord(String searchWord)
-    {
+    public boolean searchForWord(String searchWord) {
         // Defensive coding. This method should only be used after a successful crawl.
-        if(this.htmlDocument == null)
-        {
+        if (this.htmlDocument == null) {
             System.out.println("ERROR! Call crawl() before performing analysis on the document");
             return false;
         }
@@ -81,9 +95,7 @@ public class SpiderLeg
         return bodyText.toLowerCase().contains(searchWord.toLowerCase());
     }
 
-
-    public List<String> getLinks()
-    {
+    public List<String> getLinks() {
         return this.links;
     }
 
---
+++ src/main/java/com/mackleaps/spiders/SpiderTest.java
@@ -0,0 +1,19 @@
+package com.mackleaps.spiders;
+
+/**
+ *
+ * @author Lucas Hort
+ */
+public class SpiderTest {
+
+    /**
+     * This is our test. It creates a spider (which creates spider legs) and
+     * crawls the web.
+     *
+     * @param args - not used
+     */
+    public static void main(String[] args) {
+        Spider spider = new Spider();
+        spider.search("http://arstechnica.com/", "computer");
+    }
+}
